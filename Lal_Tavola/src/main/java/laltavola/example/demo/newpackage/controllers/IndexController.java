6919d0afdbb47fa8afad4eac1d3413198b8a66f6
unknown <rivera200224@gmail.com>
2023-10-08 20:30:54 -0600

    First Commit, Add Home

---
+++ Lal_Tavola/src/main/java/laltavola/example/demo/newpackage/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package laltavola.example.demo.newpackage.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+public class IndexController {
+    @GetMapping(value = "/home")
+    public String index(){
+        return "index";
+    }
+  
+}
